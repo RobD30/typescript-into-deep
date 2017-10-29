2b48e7032379651df77e5b9be0425776daf80d9a
Robert Dickerson <rdickeriii@gmail.com>
2017-10-29 16:45:31 -0700

    Refactor: Next challenge.
    end_of_line

---
+++ course-scratch.js
@@ -0,0 +1,9 @@
+interface Car {
+    make: string;
+    model: string;
+    year: number;
+};
+
+let myCar: Car = {make: 'Honda', model: 'Accord', year: 92};
+let amandasCar: Car = {make: 'Ford', model: 'Monster truck', year: 2016};
+
