bcad3c87569d1b9baf0bae7e9d33a64adc83f236
Robert Dickerson <rdickeriii@gmail.com>
2017-10-29 17:21:16 -0700

    Refactor: added interfaces and fixed problems
    end_of_line

--- exercises/accounts/src/account-manager.ts
+++ exercises/accounts/src/account-manager.ts
@@ -1,5 +1,23 @@
+interface User {
+  email: string;
+  password: string;
+}
+
+interface ConfimedUser {
+    email: string;
+  password: string;
+  isActive: boolean;
+}
+
+interface Admin {
+    email: string;
+  password: string;
+  isActive: boolean;
+  adminSince: Date;
+}
+
 export class AccountManager {
-  users = new Array();
+  users: User[] = new Array();
 
   /**
    * Create a new user account
@@ -8,7 +26,7 @@ export class AccountManager {
    * @return the new user account. An admin must activate it using activateNewUser
    * @see this.activateNewUser
    */
-  register(email, password) {
+  register(email: string, password: string): User {
     if(!email) throw 'Must provide an email';
     if(!password) throw 'Must provide a password';
     let user = { email, password };
@@ -22,10 +40,11 @@ export class AccountManager {
    * @param userToApprove Newly-registered user, who is to be activated
    * @return the updated user object, now activated
    */
-  activateNewUser(approver, userToApprove) {
+  activateNewUser(approver: Admin, userToApprove: User): ConfimedUser {
     if (!approver.adminSince) throw "Approver is not an admin!";
-    userToApprove.isActive = true;
-    return userToApprove;
+    let toConfirm = userToApprove as ConfimedUser;
+    toConfirm.isActive = true;
+    return toConfirm;
   }
 
   /**
@@ -34,7 +53,7 @@ export class AccountManager {
    * @param user an active user who you're making an admin
    * @return the updated user object, now can also be regarded as an admin
    */
-  promoteToAdmin(existingAdmin, user) {
+  promoteToAdmin(existingAdmin: boolean, user: any) {
     if (!existingAdmin.adminSince) throw "Not an admin!";
     if (user.isActive !== true) throw "User must be active in order to be promoted to admin!";
     user.adminSince = new Date();
